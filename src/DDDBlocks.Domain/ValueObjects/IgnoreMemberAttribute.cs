c00cee4c779a6938da6b17824a6c850e02b2b016
Vladyslav Horbachov <0681813464.dev@gmail.com>
2021-11-30 12:31:09 +0200

    feat(Domain): Add base for value objects

---
+++ src/DDDBlocks.Domain/ValueObjects/IgnoreMemberAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDDBlocks.Domain.ValueObjects
+{
+    /// <summary>
+    /// Shows, that current property or field will be ignored in ValueObject comparison.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class IgnoreMemberAttribute : Attribute
+    {
+    }
+}
