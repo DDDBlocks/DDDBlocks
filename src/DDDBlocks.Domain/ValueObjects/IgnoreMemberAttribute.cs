9094ef7d95cc95c7540b30edd8825601f25b3c87
Vladyslav Horbachov <0681813464.dev@gmail.com>
2021-11-30 12:34:49 +0200

    style: Enable .NET 6 analyzers, update .editorconfig

--- src/DDDBlocks.Domain/ValueObjects/IgnoreMemberAttribute.cs
+++ src/DDDBlocks.Domain/ValueObjects/IgnoreMemberAttribute.cs
@@ -3,7 +3,7 @@
 namespace DDDBlocks.Domain.ValueObjects
 {
     /// <summary>
-    /// Shows, that current property or field will be ignored in ValueObject comparison.
+    /// Marks, that current property or field will be ignored in ValueObject comparison.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public sealed class IgnoreMemberAttribute : Attribute
