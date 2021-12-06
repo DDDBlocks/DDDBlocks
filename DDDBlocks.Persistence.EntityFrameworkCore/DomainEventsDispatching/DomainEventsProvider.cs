5f6f935d25e148e7ed3ae8d730783e6d54e2b336
Vladyslav Horbachov <0681813464.dev@gmail.com>
2021-12-06 10:24:34 +0200

    feat(Persistence): Add Entity Framework Core project with domain events provider.

---
+++ DDDBlocks.Persistence.EntityFrameworkCore/DomainEventsDispatching/DomainEventsProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DDDBlocks.Domain.Entities;
+using DDDBlocks.Domain.Events;
+using DDDBlocks.Infrastructure.DomainEventsDispatching;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace DDDBlocks.Persistence.EntityFrameworkCore.DomainEventsDispatching;
+
+/// <summary>
+/// Provides collection of domain events, using Entity Framework Core tracking.
+/// </summary>
+public sealed class DomainEventsProvider : IDomainEventsProvider
+{
+    private readonly DbContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DomainEventsProvider"/> class.
+    /// </summary>
+    /// <param name="context">Database context.</param>
+    public DomainEventsProvider(DbContext context)
+    {
+        _context = context;
+    }
+
+    /// <inheritdoc/>
+    public IReadOnlyCollection<IDomainEvent> GetAllDomainEvents()
+    {
+        var domainEntities = GetDomainEntities();
+
+        return domainEntities
+                .SelectMany(instance => instance.Entity.DomainEvents)
+                .ToList();
+    }
+
+    /// <inheritdoc/>
+    public void ClearAllDomainEvents()
+    {
+        var domainEntities = GetDomainEntities();
+
+        domainEntities
+            .ForEach(entity => entity.Entity.ClearDomainEvents());
+    }
+
+    /// <summary>
+    /// Provides collection of domain entities, based on Entity Framework Core tracking.
+    /// </summary>
+    /// <returns>Collection of domain entities, that have at least one event.</returns>
+    private List<EntityEntry<Entity>> GetDomainEntities()
+    {
+        var domainEntities = _context.ChangeTracker
+            .Entries<Entity>()
+            .Where(instance =>
+                    instance.Entity.DomainEvents != null &&
+                    instance.Entity.DomainEvents.Any())
+            .ToList();
+
+        return domainEntities;
+    }
+}
